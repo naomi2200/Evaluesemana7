130e0e6344fa80ea79f870555bea863210528e4a
Naomi <solanch.veliz@tecsup.edu.pe>
2026-04-29 17:43:00 -0500

    Actualización en models.py

--- cinespoiler/movies/models.py
+++ cinespoiler/movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 # ========== MODELOS EXISTENTES ==========
 class Genre(models.Model):
     name = models.CharField('Nombre', max_length=100, unique=True)
@@ -15,6 +16,7 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+    
 
 class Movie(models.Model):
     title = models.CharField('Título', max_length=200)
@@ -37,6 +39,7 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.release_year})"
+    
 
 # ========== NUEVOS MODELOS ==========
 class Room(models.Model):
